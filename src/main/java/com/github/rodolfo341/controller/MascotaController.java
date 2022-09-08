48e7a6a138d4a2074d0ced0c1bcd7e73621fcea1
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-09-07 22:14:46 -0300

    se agrega estudiantes.html

--- src/main/java/com/github/rodolfo341/controller/MascotaController.java
+++ src/main/java/com/github/rodolfo341/controller/MascotaController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.github.rodolfo341.entity.Mascota;
 import com.github.rodolfo341.service.MascotaService;
 
 @Controller
@@ -13,9 +14,16 @@ public class MascotaController {
 	private MascotaService mascotaService;
 	
 	@GetMapping({"","/","/mascotas"})
-	public String listAllMAscotas(Model model) {
-		model.addAttribute("mascotas",mascotaService.listAllMascotas());
-		return "mascotas";
+	public String listDeMAscotas(Model model) {
+		model.addAttribute("listaMascotas",mascotaService.listAllMascotas());
+		return "/mascotas";
+	}
+	
+	@GetMapping("/mascotas/nuevo")
+	public String crearMascotaFormulario(Model model) {
+		Mascota mascota = new Mascota();
+		model.addAttribute("mascota",mascota);
+		return "crear_mascota";
 	}
 	
 }
