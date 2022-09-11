9e03938e21f5cc7f924dd62bdf72031ee3e93137
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-09-10 21:48:23 -0300

    terminando

--- src/main/java/com/github/rodolfo341/controller/MascotaController.java
+++ src/main/java/com/github/rodolfo341/controller/MascotaController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.github.rodolfo341.entity.Mascota;
@@ -31,6 +32,29 @@ public class MascotaController {
 	@PostMapping("/mascotas")
 	public String saveMascota(@ModelAttribute("mascotas") Mascota mascota) {
 		mascotaService.saveMascota(mascota);
+		return "redirect:/mascotas";
+	}
+	
+	@GetMapping("/mascotas/editar/{id}")
+	public String mostrarFormularioDeEditar(@PathVariable Long id, Model model) {
+	model.addAttribute("mascota", mascotaService.obtenerMascotaPorId(id));
+	return "editar_mascota";
+	}
+	
+	@PostMapping("/mascota/{id}")
+	public String actualizarMascota(@PathVariable Long id, @ModelAttribute("mascota") Mascota mascota, Model model) {
+		Mascota mascotaExistente = mascotaService.obtenerMascotaPorId(id);
+		mascotaExistente.setId(id);
+		mascotaExistente.setNombre(mascota.getNombre());
+		mascotaExistente.setRaza(mascota.getRaza());
+		mascotaExistente.setEdad(mascota.getEdad());
+		mascotaService.actualizarMascota(mascotaExistente);		
+		return "Redirect:/mascotas";
+	}
+	
+	@GetMapping("/mascotas/{id}")
+	public String eliminarMascota(@PathVariable Long id) {
+		mascotaService.eliminarMascota(id);
 		return "redirect:/mascotas";
 	}
 	
